fbf852508fbe3f909c1e25f1471f69ce7bc2cb21
brandyn-gilbert <brandyn.gilbert@gmail.com>
2020-02-26 08:31:05 -0500

    Completed Mini Task 03 & bonus objectives

--- Week 04/Mini Tasks/BG_Wk04_JSON_mini_03.py
+++ Week 04/Mini Tasks/BG_Wk04_JSON_mini_03.py
@@ -9,7 +9,7 @@ Brandyn Gilbert
 # =============================================================================
 # # Mini task 3: 
 # #     Create a method that assembles a list of unique values for the 
-# #     project area. If you are feeling ambitious, do the same for 'status' 
+# #     'project area.' If you are feeling ambitious, do the same for 'status' 
 # #     and 'asset type' since this will come in handy during the search 
 # #     specification.
 # =============================================================================
@@ -19,15 +19,15 @@ import os.path
 from os import path
 
 def _open_csv_title():
-    # with open('PGH_Capital_Projects.csv', newline='') as csvfile:
-    with open('PGH_Short_mini2.csv', newline='') as csvfile:
+    with open('PGH_Capital_Projects.csv', newline='') as csvfile:
+    # with open('PGH_Short_mini2.csv', newline='') as csvfile:
         PGH_reader = csv.reader(csvfile)
         title = PGH_reader.__next__()
     return title
 
 def _open_csv_contents():
-    # with open('PGH_Capital_Projects.csv', newline='') as csvfile:
-    with open('PGH_Short_mini2.csv', newline='') as csvfile:
+    with open('PGH_Capital_Projects.csv', newline='') as csvfile:
+    # with open('PGH_Short_mini2.csv', newline='') as csvfile:
         PGH_reader = csv.reader(csvfile)
         contents = []
         PGH_reader.__next__()
@@ -56,9 +56,12 @@ def _printing(PGH_list, printing_keys, errors):
         print("\n***PROJECT PROFILE***")
         for i in printing_keys:
             print(i, ":", item[i])
-        print("\n\n")  
+        print("\n\n") 
+    print("________________________________________________________________")
+    print("________________________________________________________________")
     print("\nIn total, there were \"", errors,"\" errors.", sep='')
     print("\tSee the .log file in your working directory.")
+    print("________________________________________________________________")
 
 def _logMalformedProject(entry_row): 
     if entry_row['name'] == '':
@@ -85,9 +88,27 @@ def _verify_dictionary(full_dictlist):
     return i
 
 def _unique_values(full_dictlist):
-    
-    print()
-    
+    lookup = ['area','asset_type','status']
+    unique_dict = {} 
+    i = 0
+    for i_lookup in lookup:
+        for row in full_dictlist:
+            if row[i_lookup] != '':
+                if row[i_lookup] in unique_dict:
+                    unique_dict[row[i_lookup]] += 1
+                else:
+                    unique_dict.update({row[i_lookup]:1})
+                    
+        i_lookup_up = i_lookup.upper()
+        print("________________________________________________________________")
+        print("\n\nHere is a list of unique \"",i_lookup_up, \
+              "\" values:", sep='')
+        
+        for row in unique_dict:
+            print("\t",row, ":", unique_dict[row])  
+        
+        unique_dict = {}
+        
 def main():
 
     file_out = open("area_empty.log", "w")
@@ -102,8 +123,10 @@ def main():
     contents = _open_csv_contents() # list of lists
     dict_list_complete = _combine_title_contents(title, contents)
     errors = _verify_dictionary(dict_list_complete)
-    _unique_values(dict_list_complete)
     _printing(dict_list_complete, keys_to_print, errors)
+    _unique_values(dict_list_complete)
+
+    # _print_unique_values(unique_dict)
 
 if __name__ == "__main__":
     main()
