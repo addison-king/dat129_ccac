555d69ef549e4b7f9a5221c42a3fe62d321ea67f
brandyn-gilbert <58628433+brandyn-gilbert@users.noreply.github.com>
2020-04-30 20:23:35 -0400

    Final Project update

--- Final Project/BG_DAT129_Final_Project.py
+++ Final Project/BG_DAT129_Final_Project.py
@@ -13,8 +13,44 @@ from sqlite3 import Error
 
 def main():
     auth_headers = _get_token()
+    _create_db_table()
     _scrape_series(auth_headers)
 
+def _create_db_table():
+    sql_table = '''CREATE table IF NOT EXISTS TV_Series (
+                        id integer,
+                        seriesId text,
+                        seriesName text,
+                        aliases text,
+                        season integer,
+                        poster text,
+                        banner text,
+                        fanart text,
+                        status text,
+                        firstAired text,
+                        network text,
+                        networkId text,
+                        runtime text,
+                        language text,
+                        genre text,
+                        overview text,
+                        lastUpdated text,
+                        airsDayOfWeek text,
+                        airsTime text,
+                        rating text,
+                        imdbId text,
+                        zap2itId text,
+                        added text,
+                        addedBy text,
+                        siteRating text,
+                        siteRatingCount text,
+                        slug text
+                        )'''
+    connection = _db_connection()
+    c = connection.cursor()
+    c.execute(sql_table)
+    connection.commit()
+    connection.close()
 
 def _db_connection():
     try:
@@ -48,90 +84,93 @@ def _get_token():
 
     return auth_headers
 
-def _scrape_series(headers):
-    max_count = 0
+def _get_api_json(start_num, headers):
+    url = 'https://api.thetvdb.com/series/'+ str(start_num)
+    r = requests.get(url, headers=headers)
+    text = r.json()
+    return text
 
-    first_series_id = 70327
-    start_num = first_series_id
-    scraping_length = 1
-    end_num = start_num + scraping_length
-
-    while start_num < end_num:
-        max_count += 1
-        # print(start_num)
-
-        url = 'https://api.thetvdb.com/series/'+ str(start_num)
-
-        r = requests.get(url, headers=headers)
-        start_num += 1
-
-        text = r.json()
-        print(text)
-        
-        input()
-        text2 = text['data']
-      # convert the list of genres to a string  
-        genres = text2['genre']
-        genre_str = ', '.join(genres)
-        text2['genre'] = genre_str
-      # convert the list of aliases to a string
-        aliases = text2['aliases']
-        alias_str = ', '.join(aliases)
-        text2['aliases'] = alias_str
-
-
-        columns = ', '.join(str(x).replace('/', '_') for x in text2.keys())
-        values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in text2.values())
+def _db_error_id(start_num):
+    print('\nError ID -', start_num)
+    n_lis = [start_num, None, None, None, None, None, None, None, None, 
+           None, None, None, None, None, None, None, None, None, None, 
+           None, None, None, None, None, None, None, None]
+    n_tup = tuple(n_lis)
     
-   
-        res = tuple(list(text2.values()))
+    connection = _db_connection()
+    c = connection.cursor()
+    c.execute('''INSERT INTO TV_Series VALUES
+              (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',
+              n_tup)
+    connection.commit()
+    connection.close()
+
+def _db_max_value():
+    connection = _db_connection()
+    c = connection.cursor()
+    c.execute('''SELECT MAX(id)
+                 FROM TV_Series''')
+    rows = c.fetchall()
+
+    for i in rows:
+        for j in i:
+            if j == None:
+                j = 1
+            else:
+                j += 1
+    connection.close()
+
+    return j
+
+def _scrape_series(headers):
+
+    # first_series_id = 70326
+    # start_num = first_series_id
+    start_num = _db_max_value()
+    scraping_length = 80
+    end_num = start_num + scraping_length
+      
+    while start_num < end_num:
         
-        # print(type(res))
-        # print(res)
+        text = _get_api_json(start_num, headers)
+
+        if 'Error' in text.keys():
+            _db_error_id(start_num) 
+            
+            start_num += 1
+            
+                    
+        else:
+            print('\nGood ID -', start_num)
+            start_num += 1
         
-        sql_table = '''CREATE table IF NOT EXISTS TV_Series (
-                    id text,
-                    seriesId text,
-                    seriesName text,
-                    aliases null,
-                    season integer,
-                    poster text,
-                    banner text,
-                    fanart text,
-                    status text,
-                    firstAired text,
-                    network text,
-                    networkId text,
-                    runtime text,
-                    language text,
-                    genre text,
-                    overview text,
-                    lastUpdated text,
-                    airsDayOfWeek text,
-                    airsTime text,
-                    rating text,
-                    imdbId text,
-                    zap2itId text,
-                    added text,
-                    addedBy text,
-                    siteRating text,
-                    siteRatingCount text,
-                    slug text
-                    )'''
+            text2 = text['data']
+            
+          # convert the list of genres to a string  
+            genres = text2['genre']
+            genre_str = ', '.join(genres)
+            text2['genre'] = genre_str
+          # convert the list of aliases to a string
+            aliases = text2['aliases']
+            alias_str = ', '.join(aliases)
+            text2['aliases'] = alias_str
+    
+            columns = ', '.join(str(x).replace('/', '_') for x in text2.keys())
+            values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in text2.values())
         
-        columns = ', '.join("'" + str(x).replace('/', '_') + "'" for x in text2.keys())
-        sql_insert = ''''INSERT INTO %s (%s) VALUES %s''' % ('TV_Series', columns, res)
-        # print(sql_insert)
-        
-        connection = _db_connection()
-        c = connection.cursor()
-        c.execute(sql_table)
-        print('created')
-        
-        c.execute('''INSERT INTO TV_Series VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', res)
-        print('inserted')
-        connection.commit()
-        connection.close()
+            res = tuple(list(text2.values()))
+            
+                        
+            columns = ', '.join("'" + str(x).replace('/', '_') + "'" for x in text2.keys())
+            sql_insert = ''''INSERT INTO %s (%s) VALUES %s''' % ('TV_Series', columns, res)
+            
+            connection = _db_connection()
+            c = connection.cursor()
+            
+            c.execute('''INSERT INTO TV_Series VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', res)
+            print('\tData inserted')
+            connection.commit()
+            connection.close()
 
 
 
